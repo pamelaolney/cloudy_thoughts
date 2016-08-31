7777ddf003b71a31466bf07209f60d855a1238fd
pamelaolney <pjolney12@gmail.com>
2016-08-31 15:18:11 -0400

    comments are hardcoded and nested

--- db/schema.js
+++ db/schema.js
@@ -28,4 +28,31 @@ var QuestionSchema = new Schema({
 var Question = mongoose.model("Question", QuestionSchema);
 var Comment = mongoose.model("Comment", CommentSchema);
 
+
+var sam = new Question({author: "sam"})
+var jan = new Question({author: "jan"})
+var pam = new Question({author: "pam"})
+
+var comment1 = new Comment({author: "lady1", body: "comment1!!!"})
+var comment2 = new Comment({author: "lady2", body: "comment2!!!"})
+var comment3 = new Comment({author: "lady3", body: "comment3!!!"})
+var comment4 = new Comment({author: "lady3", body: "comment4!!!"})
+var comment5 = new Comment({author: "lady4", body: "comment5!!!"})
+var comment6 = new Comment({author: "lady5", body: "comment6!!!"})
+var comment7 = new Comment({author: "lady6", body: "comment7!!!"})
+
+var authors = [sam, jan, pam]
+var comments = [comment1, comment2, comment3, comment4, comment5, comment6, comment7]
+
+for(var i = 0; i < authors.length; i++){
+  authors[i].comments.push(comments[i], comments[i+3])
+  authors[i].save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("author was saved");
+    }
+  })
+}
+
 module.exports = mongoose
