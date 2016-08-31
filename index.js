018b4addf551ec96feeb2275c052bb1ffaed7a4a
pamelaolney <pjolney12@gmail.com>
2016-08-31 07:39:35 -0400

    rack cors issue settled

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
-var mongoose = require("./db/connection");
+var cors = require("cors")
+var mongoose = require("./db/schema");
 
 var Question = mongoose.model("Question");
 var Comment = mongoose.model("Comment");
@@ -7,6 +8,8 @@ var Comment = mongoose.model("Comment");
 
 var parser = require("body-parser");
 var app = express();
+
+app.use(cors());
 app.use(parser.json({extended: true}));
 app.listen(4000, function(){
   console.log("I am here on channel 4000 bae");
@@ -25,5 +28,23 @@ app.get("/questions", function(req, res){
 app.get("/questions/:author", function(req, res){
   Question.findOne({author: req.params.author}).then(function(question){
     res.json(question);
-  })
-})
+  });
+});
+
+app.post("/questions", function(req, res){
+  Question.create(req.body.question).then(function(addquestion){
+    res.json(addquestion);
+  });
+});
+
+app.put("/questions/:author", function(req, res){
+  Question.findOneAndUpdate({author: req.params.author}, req.body.question, {new: true}).then(function(updateQuestion){
+    res.json(updateQuestion);
+  });
+});
+
+app.delete("/questions/:author/delete", function(req, res){
+  Question.findOneAndRemove({author: req.params.author}).then(function(deleteQuestion){
+    res.json(deleteQuestion);
+  });
+});
